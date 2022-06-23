8a2904847af45fb45a9deb76cfe83a2a20d96c5d
Muhiddin Uzan <muhiddinuzan@outlook.com>
2022-06-23 05:18:30 +0300

    Finish

--- weather-app/src/components/Weather.vue
+++ weather-app/src/components/Weather.vue
@@ -1,30 +1,21 @@
 <template>
   <main>
-    <div class="app flex-col">
+    <div
+      class="app flex-col"
+    >
       <h1 class="text-center font-bold mt-5">Weather App</h1>
       <div class="search-box flex justify-center items-center p-7">
         <input
           type="text"
           placeholder="Search for a city..."
+          v-model="query"
+          @keypress="fetchWeather"
           class="focus:outline-none border-black bg-transparent content-center w-96 border-2 p-4 rounded-full font-bold"
         />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-9 w-9 cursor-pointer"
-          fill="black"
-          viewBox="0 0 24 24"
-          stroke="white"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
       </div>
       <div
         class="info-box bg-gray-800 shadow-lg shadow-gray-500/50 text-white font-bold"
+        v-if="typeof weather.main != 'undefined'"
       >
         <div class="location-box mt-10">
           <div class="location justify-center items-center flex border-b-2 p-4">
@@ -46,7 +37,7 @@
                 stroke-linejoin="round"
                 d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
               /></svg
-            >Ankara, TR
+            >{{ weather.name }} , {{ weather.sys.country }}
           </div>
           <div class="date justify-center items-center flex border-b-2 p-4">
             <svg
@@ -62,12 +53,12 @@
                 stroke-linejoin="round"
                 d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
               /></svg
-            >Thursday 23 June 2022
+            >{{ dateBuilder() }}
           </div>
         </div>
         <div class="weather-box">
           <div class="temp justify-center items-center flex border-b-2 p-4">
-            9°
+            {{ Math.round(weather.main.temp) }}°c
           </div>
           <div class="weather justify-center items-center flex p-4">
             <svg
@@ -83,7 +74,7 @@
                 stroke-linejoin="round"
                 d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z"
               /></svg
-            >Cloud
+            >{{ weather.weather[0].main }}
           </div>
         </div>
       </div>
@@ -98,7 +89,58 @@ export default {
   data() {
     return {
       api_key: "960b4ed2947ff8688b76dc2ddca6ca7c",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      weather: {},
     };
+  },
+  methods: {
+    fetchWeather(e) {
+      if (e.key == "Enter") {
+        fetch(
+          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+        )
+          .then((res) => {
+            return res.json();
+          })
+          .then(this.setResults);
+      }
+    },
+    setResults(results) {
+      this.weather = results;
+      this.query = "";
+    },
+    dateBuilder() {
+      let d = new Date();
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      return `${day} ${date} ${month} ${year}`;
+    },
   },
 };
 </script>
